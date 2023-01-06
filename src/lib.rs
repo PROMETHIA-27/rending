29a3458892d4c8e7a51fa223469829b6cbac9384
PROMETHIA <electriccobras@gmail.com>
2023-01-06 17:06:52 -0500

    Removed prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
-pub use prelude::*;
-
 mod bitset;
 mod commands;
 mod context;
 mod graph;
 mod named_slotmap;
 mod node;
-mod prelude;
 mod reflect;
 mod resources;
 mod spirv_iter;
 mod util;
+
+pub use commands::RenderCommands;
+pub use context::RenderContext;
+pub use graph::{RenderGraph, RenderGraphCompilation, RenderGraphError};
+pub use node::RenderNode;
+pub use reflect::{
+    compute_pipeline_from_module, module_from_source, ModuleError, PipelineError,
+    ReflectedComputePipeline, ShaderSource,
+};
+pub use resources::{PipelineStorage, RWMode, RenderResources, Texture, TextureSize};
--- src/prelude.rs
+++
@@ -1,9 +0,0 @@
-pub use crate::commands::RenderCommands;
-pub use crate::context::RenderContext;
-pub use crate::graph::{RenderGraph, RenderGraphCompilation, RenderGraphError};
-pub use crate::node::RenderNode;
-pub use crate::reflect::{
-    compute_pipeline_from_module, module_from_source, ModuleError, PipelineError,
-    ReflectedComputePipeline, ShaderSource,
-};
-pub use crate::resources::{PipelineStorage, RWMode, RenderResources, Texture, TextureSize};
